9400d4963b6fe6804e363a7b204378e5c88c27b5
Lunar Starstrum <lunar@forsaken-borders.net>
2024-10-02 17:34:19 -0500

    VarArgs correctly

--- tools/DSharpPlus.Tools.Benchmarks/Commands/MultiArgumentCommand.cs
+++ tools/DSharpPlus.Tools.Benchmarks/Commands/MultiArgumentCommand.cs
@@ -1,12 +1,11 @@
-using System.Collections.Generic;
 using System.Threading.Tasks;
 using DSharpPlus.Commands;
 using DSharpPlus.Commands.ArgumentModifiers;
 
 namespace DSharpPlus.Tools.Benchmarks.Commands;
 
-public static class MultiArgumentCommand
+public static class VarArgsCommand
 {
-    [Command("map")]
-    public static ValueTask ExecuteAsync(CommandContext context, [MultiArgument(9)] IReadOnlyList<int> number) => ValueTask.CompletedTask;
+    [Command("varargs")]
+    public static ValueTask ExecuteAsync(CommandContext context, [MultiArgument(9)] int[] number) => ValueTask.CompletedTask;
 }
